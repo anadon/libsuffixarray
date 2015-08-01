a960df5ff58b5d5d94d28a5b41dec29ae9a9c5b2
Josh Marshall <jrmarsha@mtu.edu>
2015-08-01 17:58:33 -0400

    Added documentation, proper licensing, and improved formatting.

--- suffixArray.c
+++ suffixArray.c
@@ -1,12 +1,35 @@
-//GPLv3 this properly at some point
+/* suffixarray implementation
+ *
+ * Copyright (C) 2015  Josh Marshall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <stdio.h>
-#include <pthread.h>
 
 #include "suffixArray.h"
+
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
+
+////////////////////////////////////////////////////////////////////////
+//	MACROS	////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 
 #define max(a,b) \
@@ -21,19 +44,27 @@
      _a < _b ? _a : _b; })
 
 
+////////////////////////////////////////////////////////////////////////
+//	PRIVATE FUNCTIONS	//////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
-void resizeArray(const size_t swapSubiterators[256], 
-																		const unsigned char *targetIndexIn, 
+
+/*Conditionally enlarge an array, helper function for BTWRadixSort()*/
+void resizeArray(const size_t swapSubiterators[256],
+																		const unsigned char *targetIndexIn,
 										size_t swapSublengths[256], const size_t *lengthIn,
 																								size_t **swap){
 	const unsigned char targetIndex = *targetIndexIn;
 	const size_t length = *lengthIn;
-	
-	if(swapSubiterators[targetIndex]+1 < swapSublengths[targetIndex]) return;
-	
-	size_t suggestedSize = min((swapSublengths[targetIndex] * 2 + 1),  length);
-	swap[targetIndex] = realloc(swap[targetIndex], suggestedSize * sizeof(size_t));
-	
+
+	if(swapSubiterators[targetIndex]+1 < swapSublengths[targetIndex])
+		return;
+
+	size_t suggestedSize =
+										min((swapSublengths[targetIndex] * 2 + 1), length);
+	swap[targetIndex] =
+						realloc(swap[targetIndex], suggestedSize * sizeof(size_t));
+
 #ifdef DEBUG
 	if(swap[targetIndex] == NULL){
 		fprintf(stderr, "ALLOCATION FAILED");
@@ -42,48 +73,56 @@ void resizeArray(const size_t swapSubiterators[256],
 		fprintf(stderr, "ALLOCATION SUCCEEDED\n");
 	}
 #endif
-			
+
 	swapSublengths[targetIndex] = suggestedSize;
 }
 
 
-/*
+/***********************************************************************
  * WARNING: this function is dependant on toSort containing values in
- * acending order.  Some assumptions allow it to run in O(n/2) time
- * 
- * TODO -- this implementation is still non-optimal
- */
+ * acending order.  Some assumptions allow it to run faster.
+ *
+ * This function is used to determine the start indexes for a
+ * Burrow-Wheeler Transformation when applied to the source array in a
+ * suffixArray struct.  This should only be called once during the
+ * structure's lifetime, by makeSuffixArray().
+ *
+ * It works by adding indexes sequentially, and using a radix sort to
+ * apply a BW transform.  At the end, toSetup's bwtArray will be
+ * allocated, and correctly initalized to contain the start indexes for
+ * each start of a BW array.
+ **********************************************************************/
 void *BWTRadixSort(suffixArray *toSetup){
 	size_t **storage, *storageSublengths, *storageSubiterators, **swap;
 	size_t *swapSublengths, *swapSubiterators, **tmp, *tmp2;
 	unsigned char targetIndex;
-	
+
 	const unsigned char *source = toSetup->sequence;
-	const size_t length = toSetup->length; 
-	
-	
-	
+	const size_t length = toSetup->length;
+
+
+
 	swap 								= calloc(sizeof(size_t*), 256);
 	storage 						= calloc(sizeof(size_t*), 256);
 	storageSublengths 	= calloc(sizeof(size_t), 256);
 	swapSublengths 			= calloc(sizeof(size_t), 256);
 	storageSubiterators = calloc(sizeof(size_t), 256);
-	
+
 	swapSubiterators 		= malloc(sizeof(size_t) * 256);
 
-	
+
 	//Non-comparatively sort the items
 	for(size_t h = 0; h < length; h++){
 		memset(swapSubiterators, 0, 256 * sizeof(size_t));
 		size_t earlyLoopExit = 0;
-		
+
 		for(size_t i = 0; i < 256; i++){
 			if(earlyLoopExit >= h) break;
 			for(size_t j = 0; j < storageSubiterators[i]; j++){
 				targetIndex = source[((length-1) - h) + storage[i][j]];
-				
+
 				resizeArray(swapSubiterators, &targetIndex, swapSublengths, &length, swap);
-				
+
 				swap[targetIndex][swapSubiterators[targetIndex]++] = storage[i][j];
 			}
 			earlyLoopExit += storageSubiterators[i];
@@ -97,34 +136,35 @@ void *BWTRadixSort(suffixArray *toSetup){
 			}
 			printf("\n");
 		}
-		printf("========================================================================");
+		printf("==========================================================="
+																											"=============");
 		printf("\n\n");
 		fflush(stdout);
 #endif
-			
+
 		//Since we know these are in ascending order we just take the
 		//first unsorted element since the rest pf the elements are too
 		//short to sort.  We then use this shortcut to speed copying the
 		//remainging elements. This should halve the time.
 		targetIndex = source[h];
-		
+
 		resizeArray(swapSubiterators, &targetIndex, swapSublengths, &length, swap);
-		
+
 		swap[targetIndex][swapSubiterators[targetIndex]++] = h;
-		
+
 		tmp = storage;
-		storage = swap; 
+		storage = swap;
 		swap = tmp;
-		
+
 		tmp2 = swapSubiterators;
 		swapSubiterators = storageSubiterators;
 		storageSubiterators = tmp2;
-		
+
 		tmp2 = swapSublengths;
 		swapSublengths = storageSublengths;
 		storageSublengths = tmp2;
 	}
-	
+
 	for(int i = 0; i < 256; i++)	free(swap[i]);
 	free(swap);
 	free(swapSublengths);
@@ -132,9 +172,9 @@ void *BWTRadixSort(suffixArray *toSetup){
 	free(storageSublengths);
 	swapSublengths = swapSubiterators = storageSublengths = NULL;
 	tmp = swap = NULL;
-	
+
 	toSetup->bwtArray = (size_t*) malloc(sizeof(size_t) * length);
-		
+
 	size_t placeIndex = 0;
 	for(short i = 0; i < 256; i++){
 		size_t queueLength = storageSubiterators[i];
@@ -148,73 +188,80 @@ void *BWTRadixSort(suffixArray *toSetup){
 	}
 	free(storage);
 	free(storageSubiterators);
-	
+
 	return NULL;
 }
 
 
-	/*
-	 * 
-	 * NOTE TODO FIXME: this is stupidly parallelizable; openCL this
-	 * This code is non-optimal
-	 */
+/***********************************************************************
+ * NOTE TODO FIXME: this is stupidly parallelizable; openCL this
+ * This code is non-optimal
+ *
+ * This function is used to determine the longest common sequence from
+ * the start of 2 nieghboring indexes.  This function should only be
+ * called once over the lifetime of a suffixArray from makeSuffixArray()
+ **********************************************************************/
 void *AppendIdentInit(suffixArray *toSetup){
 	const unsigned char *source = toSetup->sequence;
 	const size_t inputLength = toSetup->length;
-	
+
 	size_t *appendIdent = (size_t*) malloc(sizeof(size_t) * inputLength);
-	
-	appendIdent[0] = 0;	//can't have and first characters in common with 
+
+	appendIdent[0] = 0;	//can't have and first characters in common with
 												//nothing
 	for(size_t i = 1; i < inputLength; i++){
 		size_t maxIndex = max(toSetup->bwtArray[i-1], toSetup->bwtArray[i]);
 		appendIdent[i] = 0;//NOTE TODO FIXME this is possible in O(n) time, not O(n^2) like this
-		for(appendIdent[i] = 0; appendIdent[i] + maxIndex < inputLength; appendIdent[i]++){
-			if(source[toSetup->bwtArray[i-1] + appendIdent[i]] != source[toSetup->bwtArray[i] + appendIdent[i]])
+		for(appendIdent[i] = 0; appendIdent[i] + maxIndex < inputLength;
+																											appendIdent[i]++){
+			if(source[toSetup->bwtArray[i-1] + appendIdent[i]] !=
+													source[toSetup->bwtArray[i] + appendIdent[i]])
 				break;
 		}
 	}
-	
+
 	toSetup->appendIdent = appendIdent;
-	
+
 	return NULL;
 }
-	
-	
-suffixArray makeSuffixArray(const unsigned char* inputSequence, 
+
+
+////////////////////////////////////////////////////////////////////////
+//	PUBLIC FUNCTIONS	//////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+suffixArray makeSuffixArray(const unsigned char* inputSequence,
 																							const size_t inputLength){
-		suffixArray toReturn = {inputSequence, NULL, inputLength, NULL, NULL};
-		
-		if(inputLength == 0 || inputSequence == NULL){	exit(-1);	}
-		
-		BWTRadixSort(&toReturn);
-		
-		AppendIdentInit(&toReturn);
-		
-		toReturn.internalSequence = NULL;
-		
-		//merge threads
+	suffixArray toReturn = {inputSequence, NULL, inputLength, NULL, NULL};
+
+	if(inputLength == 0 || inputSequence == NULL){	exit(-1);	}
+
+	BWTRadixSort(&toReturn);
+	AppendIdentInit(&toReturn);
+	toReturn.internalSequence = NULL;
+
 	return toReturn;
 }
 
 
 suffixArray copySequenceToLocal(suffixArray toMod){
 	if(toMod.internalSequence != NULL) return toMod;
-	
+
 	toMod.internalSequence = malloc(sizeof(size_t) * toMod.length);
-	memcpy(toMod.internalSequence, toMod.sequence, sizeof(size_t) * toMod.length);
-	
-	suffixArray toReturn = {toMod.internalSequence, toMod.internalSequence, 
-											toMod.length, toMod.bwtArray, toMod.appendIdent};
-	
+	memcpy(toMod.internalSequence, toMod.sequence,
+																				sizeof(size_t) * toMod.length);
+
+	suffixArray toReturn = {toMod.internalSequence,
+									toMod.internalSequence, toMod.length, toMod.bwtArray,
+																										toMod.appendIdent};
+
 	return toReturn;
 }
 
-	
-void freeSuffixArray(suffixArray toFree){
-	if(toFree.internalSequence) free(toFree.internalSequence);
-	free(toFree.bwtArray);
-	free(toFree.appendIdent);
-}
-	
 
+void freeSuffixArray(suffixArray *toFree){
+	if(toFree->internalSequence) free(toFree->internalSequence);
+	free(toFree->bwtArray);
+	free(toFree->appendIdent);
+}
--- suffixArray.h
+++ suffixArray.h
@@ -1,4 +1,21 @@
-//GPLv3 this properly at some point
+/* suffixarray header for programs to link against
+ *
+ *   Copyright (C) 2015  Josh Marshall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #ifndef SUFFIX_ARRAY_H
 #define SUFFIX_ARRAY_H
@@ -14,24 +31,38 @@
 
 typedef struct suffixArray{
 		/*This points to the sequence in memory.  The memory the sequence
-	 * is in may not belong to this object, but there is a function 
+	 * is in may not belong to this object, but there is a function
 	 * available to make a  copy in local memory*/
 	const unsigned char *sequence;
 	unsigned char *internalSequence;
 	const size_t length;
-	
+
 	size_t *bwtArray;
 	size_t *appendIdent;
 }suffixArray;
 
 
+/***********************************************************************
+ * Create a suffixArray from a passed suffixArray, effectively being a
+ * copy, except that it now owns the original sequence memory.
+ **********************************************************************/
 suffixArray copySequenceToLocal(suffixArray toMod);
-	
-	
-suffixArray makeSuffixArray(const unsigned char* inputSequence, 
+
+
+/***********************************************************************
+ * Create and initialize a suffixArray structure.
+ *
+ * It does this in the most memory efficient manner possible, so it does
+ * not copy the original sequence array.
+ **********************************************************************/
+suffixArray makeSuffixArray(const unsigned char* inputSequence,
 																							const size_t inputLength);
-	
+
+
+/***********************************************************************
+ * Destrory and free resources held by a suffixArray.
+ **********************************************************************/
 void freeSuffixArray(suffixArray toFree);
-	
+
 
 #endif
